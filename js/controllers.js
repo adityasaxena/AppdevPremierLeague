8e67d14a40f89f59437c6fd79d4d7a56adda9a96
mittalprayag25 <prayagmittaler@gmail.com>
2014-01-13 14:50:43 +0530

    random player generation

--- js/controllers.js
+++ js/controllers.js
@@ -1,3 +1,16 @@
-apl.controller('AplController', ['$scope', function($scope){
-  console.log('init');
+
+apl.controller('AplController', ['$scope', 'data', 'playerService', function($scope, data, playerService){
+  $scope.players = data.listOfPlayers;
+
+	$scope.teamA;
+	$scope.teamB;
+	$scope.currentPlayer;
+
+	$scope.getRandomPlayer = function (){
+		var player; 
+		var rand = parseInt(Math.random()*($scope.players.length-1));
+		$scope.currentPlayer = $scope.players[rand].name;
+		return $scope.players[rand];
+	}
+
 }]);
